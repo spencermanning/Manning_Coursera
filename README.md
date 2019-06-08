cb34b11ac4fa60e80aa80e49f51433857cbf9c42
Spencer Manning <spencerhmanning@gmail.com>
2019-06-08 11:47:15 -0600

    Initial repo commit with three staged basic files

---
+++ README.md
@@ -0,0 +1,1 @@
+/* Spencer Manning / Repository for Introduction to Embedded Systems Software Class / Module (Week) 1: Embedded Systems Development Components */
---
+++ stats.h
@@ -0,0 +1,40 @@
+/******************************************************************************
+ * Copyright (C) 2017 by Alex Fosdick - University of Colorado
+ *
+ * Redistribution, modification or use of this software in source or binary
+ * forms is permitted as long as the files maintain this copyright. Users are 
+ * permitted to modify this and use it to learn about the field of embedded
+ * software. Alex Fosdick and the University of Colorado are not liable for any
+ * misuse of this material. 
+ *
+ *****************************************************************************/
+/**
+ * @file Week (Module) 1 Application Assignment
+ * @brief Header file for the c file that runs the capabilities as listed in Week 1 assignement
+ *
+ * Prints statistics to the command line with c-programming operators. Compiles with GCC and executes from command line. Version control wth git version control.
+ *
+ * @author Spencer Manning
+ * @date 06/08/2019
+ *
+ */
+#ifndef __STATS_H__
+#define __STATS_H__
+
+/* Add Your Declarations and Function Comments here */ 
+
+/**
+ * @brief <Add Brief Description of Function Here>
+ *
+ * <Add Extended Description Here>
+ *
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ * @param <Add InputName> <add description here>
+ *
+ * @return <Add Return Informaiton here>
+ */
+
+
+#endif /* __STATS_H__ */
